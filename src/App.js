4ac3229bdb993953172651d7a4e174962b1bd917
webina <web.tangoan@frogasia.com>
2021-01-24 00:19:35 +0800

    feat(home): add homepage images

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import React from 'react';
 import logo from './logo.svg';
+import headshot from './images/headshot.png';
+//import profile from './images/web.gif';
+//import loading from './loading.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          WELCOME
         </p>
         <a
           className="App-link"
@@ -15,9 +18,13 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+            <img src={headshot} className="App-headshot" alt="profile" />
         </a>
       </header>
+      <footer>
+        <p>created by yours truly using </p>
+        <img src={logo} className="App-logo" alt="logo"/>
+      </footer>
     </div>
   );
 }
